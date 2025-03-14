eccf5a6fbd522cc880366afcee1ad9eb60b9541b
@koudilfan <nouserdetecteditsme@gmail.com>
2025-03-14 14:08:46 +0100

    fixing model issues

--- Backend/myapp/urls.py
+++ Backend/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import get_all_users
+from .views import get_all_users,add_user
 
 urlpatterns = [
     path('users/', get_all_users, name='get_all_users'),
+        path('api/users/add/', add_user, name='add_user'),
+
 ]
