2dd77648f21d929ed93d57836fa7053e34673587
@koudilfan <nouserdetecteditsme@gmail.com>
2025-03-14 20:50:19 +0100

    Your commit message

--- Backend/myapp/urls.py
+++ Backend/myapp/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import get_all_users,add_user
+from .views import user_list
 
 urlpatterns = [
-    path('users/', get_all_users, name='get_all_users'),
-        path('api/users/add/', add_user, name='add_user'),
-
+    path('users/', user_list, name='user-list'),
 ]
