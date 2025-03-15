237e624f8fa2d048d496dc491258b5cf63ac9787
Remil-Maha <124187584+Remil-Maha@users.noreply.github.com>
2025-03-15 15:35:54 +0100

    Update views.py

--- Backend/myapp/views.py
+++ Backend/myapp/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
-
+# crud User
 @api_view(['GET', 'POST'])
 def user_list(request):
     if request.method == 'GET':
