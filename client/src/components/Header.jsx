3e9d482bf4ba9c9773499aa693f2b8b57b0709c3
AminaTach <121464221+AminaTach@users.noreply.github.com>
2025-03-15 13:59:48 +0100

    fix home

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import pfp from "../assets/pfp.jpg";
 function Header() {
   return (
-    <div className="flex justify-end items-center p-2 mr-16">
+    <div className="flex justify-end  items-center px-2 ">
       <div className="flex items-center gap-5">
       <img
           src={pfp}
           alt="Profile"
-          className="w-12 h-12 rounded-full border-2 border-gray-300"
+          className="w-10 h-10 rounded-full border-2 border-gray-300"
         />
         <span className="text-myblack font-jakarta">Remil Maha</span>
 
--- client/src/pages/Volunteer/Home.jsx
+++ client/src/pages/Volunteer/Home.jsx
@@ -8,23 +8,32 @@ function HomePage() {
   return (
     <div className="font-lora flex bg-[#F9F9F9] w-screen h-screen overflow-hidden ">
       {/* Sidebar with fixed width */}
-      <SideBar className="w-64 min-w-[16rem] h-full bg-white shadow-lg" />
+      <SideBar className="w-64 min-w-[16rem] h-full  shadow-lg" />
 
       {/* Main content area */}
 
       <div className="flex-1 flex flex-col overflow-scroll">
-        <Header />
-        <div className="  overflow-visible">
 
-          <SearchFilters className='mr-12 sticky top-0 bg-white z-10' />
+        <div className="fixed top-0 bg-[#F9F9F9] left-[18.6%] xl:w-[calc(100%-16rem)] z-10 p-4">
+          <Header />
+          <SearchFilters />
 
-          <ONGSlider className="w-full " />
+        </div>
+        <div 
+        className=" overflow-x-hidden mt-36"
+        style={{
+          scrollbarWidth: "none", // Firefox
+          msOverflowStyle: "none", // Internet Explorer & Edge
+        }}>
+
+          <ONGSlider />
 
           <p className='font-jakarta text-[24px] font-bold ml-12 text-myblack'>Current Campaigns You Can Contribute To</p>
 
           <DonationGrid />
         </div>
       </div>
+
     </div>
   );
 };
